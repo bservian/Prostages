b89a7c5525c15fe84ec6e9d5644ea3b537d6c7c8
Benjamin Servian <bservian@iutbayonne.univ-pau.fr>
2022-01-06 19:26:12 +0100

    Creation d'entreprise/Formations via Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,44 +12,153 @@ use App\Entity\Stage;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
-
+    {     
         //creation d'un generateur de données Faker
         $faker = \Faker\Factory::create('fr_FR');
-
-        //-----------FORMATIONS-------------------//
        
-        $formation = new Formation();
-        $formation->setNomCourt("DUT_Info");
-        $formation->setNomLong("DUT_Informatique");
-        $manager->persist($formation);
+        //---------ENTREPRISES-----------------//         
+        $e1 = new Entreprise();
+        $e1->setNom($faker->company());
+        $e1->setAdresse($faker->address());
+        $e1->setActivite($faker->catchPhrase());
+        $e1->setSiteWeb($faker->domainName());
+            
+        $e2 = new Entreprise();
+        $e2->setNom($faker->company());
+        $e2->setAdresse($faker->address());
+        $e2->setActivite($faker->catchPhrase());
+        $e2->setSiteWeb($faker->domainName());
+         
+        $e3 = new Entreprise();
+        $e3->setNom($faker->company());
+        $e3->setAdresse($faker->address());
+        $e3->setActivite($faker->catchPhrase());
+        $e3->setSiteWeb($faker->domainName());
 
+        $e4 = new Entreprise();
+        $e4->setNom($faker->company());
+        $e4->setAdresse($faker->address());
+        $e4->setActivite($faker->catchPhrase());
+        $e4->setSiteWeb($faker->domainName());
 
-        
-        //---------ENTREPRISES-----------------//
-        $nbEntreprises = 10;
-       
-        for ($i=1 ;$i <= $nbEntreprises ; $i++ ) {
-            // Création d'une nouvelle entreprise
-            $entreprise = new Entreprise();
-            // Génération du nom
-            $entreprise->setNom($faker->company());
-            // Génération de l'adresse
-            $entreprise->setAdresse($faker->address());
-            // Génération de l'activite de l'entreprise
-            $entreprise->setActivite($faker->catchPhrase());
-            // Génération du site web de l'entreprise
-            $entreprise->setSiteWeb($faker->companyEmail());
-           
+        $e5 = new Entreprise();
+        $e5->setNom($faker->company());
+        $e5->setAdresse($faker->address());
+        $e5->setActivite($faker->catchPhrase());
+        $e5->setSiteWeb($faker->domainName());
+
+        $e6 = new Entreprise();
+        $e6->setNom($faker->company());
+        $e6->setAdresse($faker->address());
+        $e6->setActivite($faker->catchPhrase());
+        $e6->setSiteWeb($faker->domainName());
+
+        $e7 = new Entreprise();
+        $e7->setNom($faker->company());
+        $e7->setAdresse($faker->address());
+        $e7->setActivite($faker->catchPhrase());
+        $e7->setSiteWeb($faker->domainName());
+
+        $e8 = new Entreprise();
+        $e8->setNom($faker->company());
+        $e8->setAdresse($faker->address());
+        $e8->setActivite($faker->catchPhrase());
+        $e8->setSiteWeb($faker->domainName());
+         
+        // Regrouper les objets "Entreprise" dans un tableau pour pouuvoir s'y référer 
+        // moment de la création d'un stage.
+        $tabEntreprise = array($e1,$e2,$e3,$e4,$e5,$e6,$e7,$e8);
+         
+        foreach ($tabEntreprise as $entreprise) {
             // Enregistrememnt de l'entreprise crée
             $manager->persist($entreprise);
         }
-        
+       
+        //-----------FORMATIONS-------------------//
+       
+        $f1 = new Formation();
+        $f1->setNomCourt("DUT_Info");
+        $f1->setNomLong("DUT_Informatique");
+     
+
+        $f2 = new Formation();
+        $f2->setNomCourt("DUT_TC");
+        $f2->setNomLong("DUT Techniques de Commercialisation");
+     
+
+        $f3 = new Formation();
+        $f3->setNomCourt("LP CAA");
+        $f3->setNomLong("LP Commercialisation Agrodistribution et Agroalimentaire");
+  
+
+        $f4 = new Formation();
+        $f4->setNomCourt("BUT_Info");
+        $f4->setNomLong("BUT_Informatique");
+ 
+
+        $f4 = new Formation();
+        $f4->setNomCourt("LP Evénementiel");
+        $f4->setNomLong("LP Evénementiel");
 
 
+        $f5 = new Formation();
+        $f5->setNomCourt("LP MN");
+        $f5->setNomLong("LP Métiers du Numérique");
+ 
 
+        $tabFormation = array($f1,$f2,$f3,$f4,$f5);
+         
+        foreach ($tabFormation as $formation) {
+            // Enregistrement de la formation créée
+            $manager->persist($formation);
+        }
+
+        //---------STAGES-----------------//
+        $nbStagesAGenerer = $faker->numberBetween($min = 0, $max = 3);
+
+        for($i=0 ; $i<=4 ;$i++) {
+            //création de plusieurs stage par formations
+            for($numStage=0 ; $numStage < $nbStagesAGenerer ; $numStage++) {
+                $stage = new Stage();
+                $i = 0;
+                $stage->setTitre($faker->numerify('Stage ###')); //$faker->jobTitle() ne marche pas
+                $stage->setMission($faker->realText($maxNbChars = 5000, $indexSize = 2));
+                $stage->setEmail($faker->companyEmail());
+                // Creation de la relation Stage -> Formation
+                $stage->addFormation($tabFormation[i]);
+                // Définir et maj l'Entreprise
+                $numEntreprise = $faker->numberBetween($min = 0, $max = 7);
+
+                //creation de la relation Stage --> Entreprise
+                $stage->setEntreprise($tabEntreprise[$numEntreprise]);
+                // Creation relation Entreprise --> Stage
+                $tabEntreprise[$numEntreprise]->addStage($stage);
+
+                //Persister les objets modifiés
+                $manager->persist($stage);
+                $manager->persist($tabEntreprise[$numEntreprise]);
+                }
+        }
+    /*    //---------STAGES-----------------//
+        for ($numStage=0 ;$numStage <= 2 ; $numStage++ ) {
+            $stage = new Stage();
+            $stage->setTitre($faker->jobTitle());
+            $stage->setMission($faker->realText($maxNbChars = 5000, $indexSize = 2));
+            $stage->setEmail($faker->companyEmail());
+            // Création relation Stage --> Formation
+            $stage->addFormation($formation);
+            // Définir et maj l'Entreprise
+            $numEntreprise = $faker->numberBetween($min = 28, $max = 21));
+            //creation de la relation Stage --> Entreprise
+            $stage->setEntreprise($tabEntreprise[$numEntreprise]);
+            // Creation relation Entreprise --> Stage
+            $tabEntreprise[$numEntreprise]->addStage($stage);
+
+            //Persister les objets modifiés
+            $manager->persist($stage);
+            $manager->persist($tabEntreprise[$numEntreprise]);
+        }
+    */
         //Envoyer les données
         $manager->flush();
     }
