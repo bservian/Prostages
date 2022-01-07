572cf5e50a7f2e7045c7c8bff8c21e8e862f52ba
Benjamin Servian <bservian@iutbayonne.univ-pau.fr>
2022-01-07 18:30:35 +0100

    Ecriture du code du controleur pour récupérer les ressources en BD

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Stage;
+use App\Entity\Formation;
+use App\Entity\Entreprise;
 
 class ProstagesController extends AbstractController
 {
     public function index(): Response
     {
-        return $this->render('prostages/index.html.twig', [
-            'controller_name' => 'ProstagesController',
-        ]);
+        //recuperer le repository de l'netité Ressource
+        $repositoryRessource = $this->getDoctrine()->getRepository(Stage::class);
+        //recuperer  les ressources enregistrées en BD
+        $stages = $repositoryRessource->findAll();
+        //Envoyer les ressources récupérées à la vue chargée de les afficher
+        return $this->render('prostages/index.html.twig',
+         ['stages' => $stages]);
     }
 
     public function entreprise(): Response
